40b3f00e14500c948a4ee5d2f39df78f777acc86
spamulapati <spamulapati@FHLT0014.xeno.com>
2019-10-11 09:35:51 +0530

    Sample Project Commit

---
+++ TestScript/sample_Test/InvokeBrowser.java
@@ -0,0 +1,30 @@
+package sample_Test;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class InvokeBrowser {
+	static WebDriver driver;
+
+	@Test (priority = 00)
+	public void InvokeBrw() {
+		String path = "driver/chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", path); 
+		driver=new ChromeDriver();
+	}
+	
+	@Test (priority = 01)
+	public void printInkBrw() {
+	System.out.println("Chrome Browser Invoked");	
+	driver.manage().window().maximize();
+	Dimension abc = driver.manage().window().getSize();
+	System.out.println("Dimension of screen size :"+abc);
+	}
+	
+	@Test (priority = 02)
+	public void brwClose() {
+		driver.close();
+	}
+}
---
+++ TestScript/sample_Test/SampleTestNG.java
@@ -0,0 +1,11 @@
+package sample_Test;
+
+import org.testng.annotations.Test;
+
+public class SampleTestNG {
+
+	@Test
+	public void test_01() {
+		System.out.println("Sample TestNG");
+	}
+}
